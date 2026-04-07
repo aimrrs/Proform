edfbbed321dac40ad397281368950cc7a2d3c39d
Sriram Vengadesan <aimrrs404@gmail.com>
2026-04-07 17:32:32 +0530

    Bug.

--- pydanticModels.py
+++ pydanticModels.py
@@ -18,7 +18,7 @@ class AddCourseDepartmentItems (BaseModel):
 
 class UpdateProfileItems (BaseModel):
     year: int | None = None
-    linked_link: str | None = None
+    linkedin_link: str | None = None
     github_link: str | None = None
 
 #class GoogleToken (BaseModel):
